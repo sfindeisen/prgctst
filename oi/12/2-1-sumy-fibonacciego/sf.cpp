a2f0abb64f9d098c46b66ddf61382213268d30f1
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2018-04-25 12:47:26 +0200

    oi 12: sumy fibonacciego

--- oi/12/2-1-sumy-fibonacciego/sf.cpp
+++ oi/12/2-1-sumy-fibonacciego/sf.cpp
@@ -1,112 +1,136 @@
-/**
- *   2007-11-19
- *   STF
- */
-
-#include <cstring>
 #include <iostream>
+#include <vector>
+
+typedef int Int;
+typedef long long Long;
+typedef std::vector<Int> VInt;
+
+#define ssize(c) ((Long)((c).size()))
 
 using namespace std;
 
-const long MaxLength = 1000000;
-
-void foldRight(char * fibo, long index) {
-    long i = index;
-    while ((1 == fibo[i]) && (1 == fibo[i+1])) {
-        //cout << "FR(" << i << ") ";
-        fibo[i]   =  0;
-        fibo[i+1] =  0;
-        fibo[i+2] += 1;
-        i += 2;
-    }
-    //cout << "\n";
+void main_start() {
+    ios_base::sync_with_stdio(0);
 }
 
-void inc(char * fibo, long index) {
-    fibo[index] += 1;
-    //cout << "inc[" << index << "] => " << (int) fibo[index] << "\n";
-    if (1 == fibo[index]) {
-        // cout << "-- 1\n";
-        if (1 == fibo[index + 1])
-            foldRight(fibo, index);
-        else if ((1 <= index) && (1 == fibo[index - 1]))
-            foldRight(fibo, index - 1);
-    } else if (2 == fibo[index]) {
-        // cout << "-- 2\n";
-        if (0 == index) {
-            // cout << "-- -- 0\n";
-            fibo[0] = 0;
-            fibo[1] = 1;
-            foldRight(fibo, 1);
-        } else if (1 == index) {
-            // cout << "-- -- 1\n";
-            fibo[0] = 1;
-            fibo[1] = 0;
-            fibo[2] = 1;
-            foldRight(fibo, 2);
-        } else {
-            // cout << "-- -- daleko\n";
-            fibo[index]     = 0;
-            fibo[index + 1] = 1;
-            foldRight(fibo, index + 1);
+VInt f1, f2, fsum;
 
-            inc(fibo, index - 2);
-            //long k = index - 2;
-            //fibo[k] += 1;
-        }
-    }
-}
-
-void wczytaj(char * fibo) {
-    long i = 0, k = 0;
+VInt wczytaj() {
+    int i=0, k=0;
     cin >> i;
-    // cout << i << " digits: ";
+    VInt v;
 
-    for (int j = 0; j < i; j++) {
+    for (int j=0; j<i; ++j) {
         cin >> k;
-        fibo[j] = k;
-        // cout << (int) fibo[j] << " ";
+        v.push_back(k);
     }
-    // cout << '\n';
+
+    return v;
 }
 
-void czytaj_i_dodawaj(char * fibo) {
-    long i = 0, k = 0;
-    cin >> i;
-    // cout << "czytaj_i_dodawaj: digits: " << i << "\n";
-
-    for (long j = 0; j < i; j++) {
-        cin >> k;
-        if (1 == k) {
-            // cout << "czytaj_i_dodawaj: " << j << "\n";
-            inc(fibo, j);
-        }
-    }
-}
-
-void wypisz_wynik(char * fibo, long len) {
-    long end = len - 1;
-    while ((0 <= end) && (0 == fibo[end]))
-        end--;
-    // cout << "Wynik: end=" << end << "\n";
-    if (end < 0)
+void wypisz(const VInt& fibo) {
+    int k = fibo.size()-1;
+    for (; (0 <= k) && (0 == fibo[k]); --k);
+    if (k < 0)
         cout << "1 0";
     else {
-        cout << (end + 1) << " ";
-        for (long i = 0; i <= end; i++)
-            cout << (int) fibo[i] << " ";
+        cout << (k+1);
+        for (int i=0; i<=k; ++i)
+            cout << " " << fibo[i];
     }
-    cout << '\n';
+    cout << endl;
+}
+
+// eliminacja 2
+// kazda 2 jest otoczona z obydwu stron 0 (poza byc moze najmniej znaczaca)
+void elim2(VInt& fibo) {
+    for (int i=0; i < ssize(fibo)-1; ++i) {
+        if (2 == fibo[i]) {
+            if (0 == i) {
+                // 0 2 $
+                fibo[1] = 1;
+                fibo[0] = 0;
+            } else {
+                // 0 2 ..
+                fibo[i+1] += 1;
+                fibo[i]    = 1;
+                fibo[i-1] -= 1;
+            }
+        }
+    }
+}
+
+// eliminacja -1
+// nie ma dwoch -1 obok siebie
+// na lewo od kazdej -1 stoi +1
+void elim1(VInt& fibo) {
+    for (int i=ssize(fibo)-2; 0<=i; --i) {
+        if (-1 == fibo[i]) {
+            if (0 == fibo[i+1]) {
+                // 1 0 -1
+                fibo[i+2] = 0;
+                fibo[i+1] = 1;
+                fibo[i]   = 0;
+            } else {
+                // 1 -1
+                if (0 == i) {
+                    // 1 -1 $
+                    fibo[1] = 0;
+                    fibo[0] = 1;
+                } else {
+                    // 1 -1 ..
+                    if (1 == fibo[i-1]) {
+                        // 1 -1 1
+                        if (1 == i) {
+                            // 1 -1 1 $
+                            fibo[2] = 0;
+                            fibo[1] = 1;
+                            fibo[0] = 0;
+                        } else {
+                            // 1 -1 1 ..
+                            fibo[i+1]  = 0;
+                            fibo[i]    = 1;
+                            fibo[i-1]  = 1;
+                            fibo[i-2] -= 1;
+                        }
+                    } else {
+                        // 1 -1 0
+                        fibo[i+1] = 0;
+                        fibo[i]   = 0;
+                        fibo[i-1] = 1;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void zwin(VInt& fibo) {
+    const int z = fibo.size() - 1;
+
+    for (int i=z; 1<=i; --i) {
+        for (int j=i; ((j<z) && (1==fibo[j]) && (1==fibo[j-1])); j+=2) {
+            fibo[j+1] = 1;
+            fibo[j]   = 0;
+            fibo[j-1] = 0;
+        }
+    }
 }
 
 int main() {
-    const long len = MaxLength + 5;
-    char fibo[len];
-    memset(fibo, 0, len);
+    main_start();
 
-    wczytaj(fibo);
-    czytaj_i_dodawaj(fibo);
-    wypisz_wynik(fibo, len);
+    f1 = wczytaj();
+    f2 = wczytaj();
 
+    const int sz = max(ssize(f1), ssize(f2));
+    fsum.assign(sz+5, 0);
+    for (int i=0; i<sz; ++i)
+        fsum[i] = ((i<ssize(f1)) ? f1[i] : 0) + ((i<ssize(f2)) ? f2[i] : 0);
+    elim2(fsum);
+    elim1(fsum);
+    zwin(fsum);
+
+    wypisz(fsum);
     return 0;
 }
